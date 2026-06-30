80a52cac9ab403f98bcf829a5e3c214fd65bf6a7
max bittker <maxbittker@gmail.com>
2026-06-30 13:20:48 -0400

    Updates

---
+++ _astro/Bookshelf.D0S45pih.js
@@ -0,0 +1,1 @@
+import{G as p}from"./Bookshelf.oC25qUBU.js";import"./jsx-runtime.BftctW7E.js";import"./index.DJO9vBfz.js";export{p as default};
---
+++ _astro/Header.C6wOJfGM.js
@@ -0,0 +1,9 @@
+import{j as e}from"./jsx-runtime.BftctW7E.js";import"./index.DJO9vBfz.js";import{s as p,W as r}from"./Wrap.CyMt_Cnr.js";let x={color:"black",textDecoration:"none",padding:"0px  3px",position:"relative",zIndex:100,textShadow:`
+  -1px -1px  #f4eeef,
+   1px -1px  #f4eeef,
+   -1px 1px  #f4eeef,
+  1px 1px  #f4eeef,
+  -2px -2px  #f4eeef,
+   2px -2px  #f4eeef,
+   -2px 2px  #f4eeef,
+  2px 2px  #f4eeef`};const n=()=>(typeof window<"u"&&(window.l=9),p(),e.jsxs("div",{className:"header",children:[e.jsx(r,{n:3,pack:!0,children:e.jsx("a",{href:"/",style:x,children:"max bittker"})}),e.jsx(r,{n:3,pack:!0,children:e.jsx("a",{href:"/projects/",style:x,children:"projects"})}),e.jsx(r,{n:3,pack:!0,children:e.jsx("a",{href:"/blog/",style:x,children:"blog"})}),e.jsx(r,{n:3,pack:!0,children:e.jsx("a",{href:"/roll/",style:x,children:"roll"})}),e.jsx(r,{n:3,pack:!0,children:e.jsx("a",{href:"/bookshelf/",style:x,children:"shelf"})})]}));export{n as default};
